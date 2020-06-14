251d2d15dc0b39f34bcd07358ac507114eff2424
Michael Nascimento <michael.nsc@outlook.com>
2020-06-14 11:02:00 -0300

    Fixed Search Results Links

--- src/components/Search/SearchResultItem.jsx
+++ src/components/Search/SearchResultItem.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { createSlug } from '../../utils';
 
 const SearchResultItem = ({ productData }) => {
+  const slug = createSlug(productData.name);
   return (
-    <article className="shopping-cart__item">
-      <div className="col-6 shopping-cart__thumb">
-        <img
-          className="product__image"
-          src={productData.image}
-          alt={productData.name}
-        />
-      </div>
-      <div className="col-6">
-        <h3 className="product__title">{productData.name}</h3>
-      </div>
-    </article>
+    <Link
+      to={{
+        pathname: `/product/${slug}`,
+        state: productData,
+      }}
+    >
+      <article className="shopping-cart__item">
+        <div className="col-6 shopping-cart__thumb">
+          <img
+            className="product__image"
+            src={productData.image}
+            alt={productData.name}
+          />
+        </div>
+        <div className="col-6">
+          <h3 className="product__title">{productData.name}</h3>
+        </div>
+      </article>
+    </Link>
   );
 };
 
